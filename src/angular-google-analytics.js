c657895311cc339ab00c1ade6913ef5a4ff7693d
Jonathan Meiss <jonathan@meiss.fr>
2013-05-16 17:08:47 +0200

    Fix provider to get it working with Angular 1.1.4

--- src/angular-google-analytics.js
+++ src/angular-google-analytics.js
@@ -1,81 +1,80 @@
-/* global angular, console */
+/* global angular */
 
 'use strict';
 
-angular.module('angular-google-analytics', [])
-    .provider('Analytics', function() {
-        var created = false,
-            trackRoutes = true,
-            accountId;
+angular.module('google-analytics', [])
+  .provider('Analytics', function () {
+    // config methods
+    this.setAccount = function(accountId) {
+      this.accountId = accountId;
+      return true;
+    };
+    this.trackPages = function(shouldTrackPages) {
+      this.shouldTrackPages = shouldTrackPages;
+      return true;
+    };
 
-          this._logs = [];
+    this.$get = ['$document', '$rootScope', '$location', '$window', function($document, $rootScope, $location, $window) {
+      var accountId = this.accountId,
+          shouldTrackPages = this.shouldTrackPages;
 
-          // config methods
-          this.setAccount = function(id) {
-              accountId = id;
-              return true;
-          };
-          this.trackPages = function(doTrack) {
-              trackRoutes = doTrack;
-              return true;
-          };
+      // private methods
+      function _createScriptTag() {
+        // inject the google analytics tag
+        if (!accountId) {
+          return;
+        }
 
-        // public service
-        this.$get = ['$document', '$rootScope', '$location', '$window', function($document, $rootScope, $location, $window) {
-          // private methods
-          function _createScriptTag() {
-            // inject the google analytics tag
-            if (!accountId) return;
-            $window._gaq = [];
-            $window._gaq.push(['_setAccount', accountId]);
-            if (trackRoutes) $window._gaq.push(['_trackPageview']);
-            (function() {
-              var document = $document[0];
-              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-              ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-            created = true;
-          }
-          this._log = function() {
-            // for testing
-            this._logs.push(arguments);
-          };
-          this._trackPage = function(url) {
-            if (trackRoutes && $window._gaq) {
-              $window._gaq.push(['_trackPageview', url]);
-              this._log('_trackPageview', arguments);
-            }
-          };
-          this._trackEvent = function(category, action, label, value) {
-            if ($window._gaq) {
-              $window._gaq.push(['_trackEvent', category, action, label, value]);
-              this._log('trackEvent', arguments);
-            }
-          };
+        var document  = $document[0];
+        var ga        = document.createElement('script');
+        var s         = document.getElementsByTagName('script')[0];
+        
+        $window._gaq = [];
+        $window._gaq.push(['_setAccount', accountId]);
 
-          // creates the ganalytics tracker
-          _createScriptTag();
+        if (shouldTrackPages) {
+          $window._gaq.push(['_trackPageview']);
+        }
 
-          var me = this;
+        ga.type   = 'text/javascript';
+        ga.async  = true;
+        ga.src    = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(ga, s);
+      }
 
-          // activates page tracking
-          if (trackRoutes) $rootScope.$on('$routeChangeSuccess', function(scope, current, previous) {
-            me._trackPage($location.path());
-          });
+      this._trackPage = function(url) {
+        if (shouldTrackPages && $window._gaq) {
+          $window._gaq.push(['_trackPageview', url]);
+        }
+      };
 
-          return {
-                _logs: me._logs,
-                trackPage: function(url) {
-                    // add a page event
-                    me._trackPage(url);
-                },
-                trackEvent: function(category, action, label, value) {
-                    // add an action event
-                    me._trackEvent(category, action, label, value);
-                }
-            };
-        }];
+      this._trackEvent = function(category, action, label, value) {
+        if ($window._gaq) {
+          $window._gaq.push(['_trackEvent', category, action, label, value]);
+        }
+      };
 
-    });
+      // creates the ganalytics tracker
+      _createScriptTag();
 
+      var me = this;
+
+      // activates page tracking
+      if (shouldTrackPages) {
+        $rootScope.$on('$viewContentLoaded', function(scope, current, previous) {
+          me._trackPage($location.path());
+        });
+      }
+
+      return {
+        trackPage: function(url) {
+          // add a page event
+          me._trackPage(url);
+        },
+        trackEvent: function(category, action, label, value) {
+          // add an action event
+          me._trackEvent(category, action, label, value);
+        }
+      };
+    }];
+  })
